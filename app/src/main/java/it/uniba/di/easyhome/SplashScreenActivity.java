087dace5d76f806a1a42e6990577251cacb62617
f-moscato <f.moscato@hotmail.com>
2020-01-08 12:32:07 +0100

    Miglioramenti Splash Screen

--- app/src/main/java/it/uniba/di/easyhome/SplashScreenActivity.java
+++ app/src/main/java/it/uniba/di/easyhome/SplashScreenActivity.java
@@ -2,7 +2,14 @@ package it.uniba.di.easyhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ActionMenuView;
+import android.widget.Button;
+
+import it.uniba.di.easyhome.ui.login.LoginActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +17,16 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                startActivity(i);
+
+            }
+        }, 3000);
+
+}}
