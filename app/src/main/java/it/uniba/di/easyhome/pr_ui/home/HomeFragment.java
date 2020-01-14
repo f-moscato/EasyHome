6b31d7917594d99ab6097f53c16e2d171e964382
MaffeiMarcp <mrcmaffei@yahoo.it>
2020-01-14 12:02:13 +0100

    inizio query case in Proprietario

--- app/src/main/java/it/uniba/di/easyhome/pr_ui/home/HomeFragment.java
+++ app/src/main/java/it/uniba/di/easyhome/pr_ui/home/HomeFragment.java
@@ -13,8 +13,21 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+import it.uniba.di.easyhome.House;
 import it.uniba.di.easyhome.R;
+import it.uniba.di.easyhome.User;
 
 public class HomeFragment extends Fragment {
 
@@ -27,14 +40,33 @@ public class HomeFragment extends Fragment {
         fab.setClickable(true);
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.pr_fragment_home, container, false);
+        final View root = inflater.inflate(R.layout.pr_fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        /*homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+
+        final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference("houses");
+
+
+        ArrayList<House> caseProprietario= new ArrayList<>();
+
+       /* rootRef.child("owner").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Query query =root.orderByChild
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        })*/
+
         return root;
     }
 }
