1cb0d2c8f822513c3b645c7e3c1c283b714602c3
MaffeiMarco <mrcmaffei@yahoo.it>
2020-01-17 16:31:39 +0100

    effetto bottoni

--- app/src/main/java/it/uniba/di/easyhome/pr_ui/home/HomeFragment.java
+++ app/src/main/java/it/uniba/di/easyhome/pr_ui/home/HomeFragment.java
@@ -72,7 +72,7 @@ public class HomeFragment extends Fragment {
                         LinearLayout lyl= new LinearLayout(getActivity());
                         lyl.setBackground(getResources().getDrawable(R.drawable.blue_border_rounded_cornwe));
                         LinearLayout.LayoutParams lly_button=new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                        
+
                         ImageView img= new ImageView(getActivity());
                         img.setImageResource(R.drawable.bill_inquilino);
                         TextView tw= new TextView(getActivity());
