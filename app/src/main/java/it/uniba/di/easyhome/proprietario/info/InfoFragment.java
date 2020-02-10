f2cc3dea35beb09f15bc8fac35bd6ee6fd4af6e5
f-moscato <f.moscato@hotmail.com>
2020-02-10 21:36:21 +0100

    Gestione fab inquilino

--- app/src/main/java/it/uniba/di/easyhome/proprietario/info/InfoFragment.java
+++ app/src/main/java/it/uniba/di/easyhome/proprietario/info/InfoFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import it.uniba.di.easyhome.ProprietarioActivity;
 import it.uniba.di.easyhome.R;
 
 public class InfoFragment extends Fragment {
@@ -18,9 +19,11 @@ public class InfoFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        FloatingActionButton fab= (getActivity().findViewById(R.id.fab_plus));
-        fab.hide();
-        fab.setClickable(false);
+
+        if(getActivity().equals(ProprietarioActivity.class)) {
+            FloatingActionButton fab = (getActivity().findViewById(R.id.fab_plus));
+            fab.hide();
+            fab.setClickable(false);}
         View root = inflater.inflate(R.layout.fragment_info, container, false);
 
 
